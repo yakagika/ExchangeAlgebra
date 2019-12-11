6dca52b82900cac3b64a986e5c1d2af16dda6822
Kaya Akagi <akagi@Kayas-MacBook-Pro.local>
2019-12-11 16:24:36 +0900

    for debug

--- src/ExchangeAlgebra.hs
+++ src/ExchangeAlgebra.hs
@@ -440,7 +440,7 @@ instance (ExBaseClass b) =>  Redundant (ExAlg b) where
         | otherwise
             = let h   = getHat b  in
                 let h'  = getHat b' in
-                case (h,h') of
+                case (trace (show (h,h')) (h, h')) of
                 (Hat, Hat) -> (v + v') :@ b
                 (Not, Not) -> (v + v') :@ b
                 (Not, Hat)  | v == v' -> Zero
